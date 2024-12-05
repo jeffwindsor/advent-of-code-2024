ce807d6d2d3eff14d60e30a927aeadf54e67b07f
Jeff Windsor <jeff.windsor@gmail.com>
2024-12-04 22:02:12 -0800

    Day 5 - Info and Parser only

---
+++ day5/print_queue.py
@@ -0,0 +1,13 @@
+from parser import parse
+
+def part1(file):
+    pass
+
+def part2(file):
+    pass
+
+if __name__ == "__main__":
+    print(f"part 1 example should be 143: {part1('example1')}")
+    # print(f"part 1: {part1('puzzle_input')}")
+    # print(f"part 2 example should be _: {part2('example2')}")
+    # print(f"part 2: {part2('puzzle_input')}")
