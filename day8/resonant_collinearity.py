e3b943e26d90ad766bdda1855fa0e44ad66c9d62
Jeff Windsor <jeff.windsor@gmail.com>
2024-12-09 11:25:43 -0800

    Day 8 - refactor to shared code.. Meh

--- day8/resonant_collinearity.py
+++ day8/resonant_collinearity.py
@@ -5,46 +5,44 @@ def is_out_of_bounds(limits, coord):
     return (coord[0] < 0 or coord[0] > limits[0] or
             coord[1] < 0 or coord[1] > limits[1])
 
-def part1(file):
-    antinodes = []
-    limits, frequency_coords = parse(file)
+def add_if_in_bounds(limits, cs, c):
+    if not is_out_of_bounds(limits, c):
+        cs.append(c)
+        return True
+    return False
 
-    def add_if_in_bounds(c):
-        if not is_out_of_bounds(limits, c):
-            antinodes.append(c)
+def count_antinodes(add_func, file):
+    nodes = []
+    limits, frequency_coords = parse(file)
 
     for _, coords in frequency_coords.items():
         for (x1, y1), (x2, y2) in combinations(coords, 2):
             dx, dy = x1 - x2, y1 - y2
-            add_if_in_bounds((x2 - dx, y2 - dy))
-            add_if_in_bounds((x1 + dx, y1 + dy))
 
-    return len(set(antinodes))
+            add_func(limits, nodes, dx, dy, x1, y1, x2, y2)
+
+    return len(set(nodes))
+
+
+def part1(file):
+    def add_two(limits, nodes, dx, dy, x1,y1, x2, y2):
+        add_if_in_bounds(limits, nodes, (x2 - dx, y2 - dy))
+        add_if_in_bounds(limits, nodes, (x1 + dx, y1 + dy))
+
+    return count_antinodes(add_two, file)
 
 
 def part2(file):
-    antinodes = []
-    limits, frequency_coords = parse(file)
+    def add_until_bounds(limits, nodes, dx, dy, x1,y1, x2, y2):
+        while add_if_in_bounds(limits, nodes, (x2, y2)):
+            x2 -= dx
+            y2 -= dy
 
-    def add_if_in_bounds(c):
-        if not is_out_of_bounds(limits, c):
-            antinodes.append(c)
-            return True
-        return False
+        while add_if_in_bounds(limits, nodes, (x1, y1)):
+            x1 += dx
+            y1 += dy
 
-    for _, coords in frequency_coords.items():
-        for (x1, y1), (x2, y2) in combinations(coords, 2):
-            dx, dy = x1 - x2, y1 - y2
-
-            while add_if_in_bounds((x2, y2)):
-                x2 -= dx
-                y2 -= dy
-
-            while add_if_in_bounds((x1, y1)):
-                x1 += dx
-                y1 += dy
-
-    return len(set(antinodes))
+    return count_antinodes(add_until_bounds, file)
 
 
 if __name__ == "__main__":
