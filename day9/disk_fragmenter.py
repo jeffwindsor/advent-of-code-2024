81aa9bc38fc05e1e0aeb560b0aa45373ce1c509e
Jeff Windsor <jeff.windsor@gmail.com>
2024-12-09 18:17:18 -0800

    Day 9

--- day9/disk_fragmenter.py
+++ day9/disk_fragmenter.py
@@ -1,14 +1,96 @@
 from parser import parse
+from itertools import repeat
 
+EMPTY_BLOCK = "."
+
+
+def checksum(disk):
+    return sum([i * int(n) for i, n in enumerate(disk) if n != EMPTY_BLOCK])
 
 def part1(file):
-    pass
+    def expand(file_blocks, free_space):
+        result = []
+        for i in range(len(file_blocks)):
+            result += repeat(str(i), file_blocks[i])
+            result += repeat(EMPTY_BLOCK, free_space[i])
+        return list(result)
+
+
+    disk = expand(*parse(file))
+    right = len(disk) - 1
+    for left in range(len(disk)):
+        if disk[left] == EMPTY_BLOCK:
+            while disk[right] == EMPTY_BLOCK:
+                right -= 1
+
+            if left >= right:
+                break
+            else:
+                disk[left] = disk[right]
+                disk[right] = EMPTY_BLOCK
+                # print(f"Move {right} to {left}")
+                # print("".join(disk))
+    return checksum(disk)
+
+
+def find_available_free_space(free_space, block_size):
+    for id, size in enumerate(free_space):
+        if block_size <= size:
+            # print(f"moves to {id}({size})")
+            # reduce free space at fid by block size now there
+            free_space[id] -= block_size
+            return id
+    return None
 
 
 def part2(file):
-    pass
+    def expand(moved_blocks, free_space):
+        result = []
+        for i in range(len(moved_blocks)):
+            for id, size in moved_blocks[i]:
+                result += repeat(str(id), size)
+            result += repeat(EMPTY_BLOCK, free_space[i])
+        return list(result)
+
+
+    file_blocks, free_space = parse(file)
+    # 2d array of moved_blocks
+    moved_blocks = [[] for _ in range(len(file_blocks))]
+
+    for id, size in reversed(list(enumerate(file_blocks))):
+        # print(f"{str(id) * size} ", end="")
+
+        # remove file blocks, leaving more free space
+        # worst case the blocks are moved back to original space
+        free_space[id] += size
+        file_blocks[id] = 0
+        move_to_id = find_available_free_space(free_space, size)
+        if move_to_id == id:
+            # insert original at front of id/index list
+            moved_blocks[move_to_id] = [(id, size)] + moved_blocks[move_to_id]
+            continue
+
+        elif move_to_id is not None:
+            # edge case of moving a block after other block at index
+            if moved_blocks[id]:
+                # reduce free space at id by size
+                free_space[id] -= size
+                # insert a free space of size to front of list [244.] => [.44.]
+                moved_blocks[id] = [(EMPTY_BLOCK, size)] + moved_blocks[id]
+
+            # add to end of id/index list
+            moved_blocks[move_to_id] += [(id, size)]
+            continue
+
+    disk = expand(moved_blocks, free_space)
+    return checksum(disk)
 
 
 if __name__ == "__main__":
-    print(f"simple : {part1('example_simple')}")
-    print(f"example: {part1('example')}")
+    print(f"1:simple  (  60): {part1('example_simple')}")
+    print(f"1:example (1928): {part1('example')}")
+    print(f"1:example (6332189866718): {part1('puzzle_input')}")
+
+    print(f"2:simple  ( 132): {part2('example_simple')}")
+    print(f"2:example (2858): {part2('example')}")
+    print(f"2:example (6353648390778): {part2('puzzle_input')}")
--- day9/parser.py
+++ day9/parser.py
@@ -4,18 +4,14 @@ def parse(file):
     with open(file, "r") as file:
         numbers = file.read().strip()
 
-    # if no free space is given for last file block,
-    # then add a zero for no following free space
-    if len(numbers) % 2 == 1:
-        numbers += '0'
+    file_blocks = list(map(int, islice(numbers, 0, None, 2)))  # first numbers
+    free_blocks = list(map(int, islice(numbers, 1, None, 2)))  # second numbers
 
-    file_block_sizes = islice(numbers, 0, None, 2)  # first numbers
-    free_spaces = islice(numbers, 1, None, 2)       # second numbers
-    pairs = zip(file_block_sizes, free_spaces)
+    if len(file_blocks) > len(free_blocks):
+        free_blocks.append(0)
 
-    # original_index, file_block_size, free_space
-    return [(i, int(fb), int(fs)) for i, (fb, fs) in enumerate(pairs)]
+    return file_blocks, free_blocks
 
 if __name__ == "__main__":
-    print(f"simple : {list(parse('example_simple'))}")
-    print(f"example: {list(parse('example'))}")
+    print(f"simple : {parse('example_simple')}")
+    print(f"example: {parse('example')}")
