8f6b093a1704a130a5a09f02ee40e1222435f743
Jeff Windsor <jeff.windsor@gmail.com>
2024-12-15 16:48:40 -0800

    14.2

--- day14/restroom_redoubt.py
+++ day14/restroom_redoubt.py
@@ -5,14 +5,14 @@ GRID_HEIGHT = 103
 SECONDS = 100
 
 
-def simulate_robots(robots, seconds, width, height):
+def simulate_robot_positions(robots, seconds, width, height):
     """Simulate robot positions after a given time."""
-    final_positions = []
+    positions = []
     for (px, py), (vx, vy) in robots:
-        new_x = (px + vx * seconds) % width
-        new_y = (py + vy * seconds) % height
-        final_positions.append((new_x, new_y))
-    return final_positions
+        x = (px + vx * seconds) % width
+        y = (py + vy * seconds) % height
+        positions.append((x, y))
+    return positions
 
 
 def count_quadrants(positions, width, height):
@@ -44,20 +44,43 @@ def calculate_safety_factor(quadrants):
     return result
 
 
-def part1(filepath, seconds, grid_width, grid_height):
+def part1(filepath, seconds, width, height):
     robots = parse_file(filepath)
-    final_positions = simulate_robots(robots, seconds, grid_width, grid_height)
-    quadrants = count_quadrants(final_positions, grid_width, grid_height)
+    final_positions = simulate_robot_positions(robots, seconds, width, height)
+    quadrants = count_quadrants(final_positions, width, height)
     safety_factor = calculate_safety_factor(quadrants)
     return safety_factor
 
 
-def part2(filepath):
-    pass
+def find_minimum_safety_factor(robots, max_seconds, width, height):
+    """Find the second with the minimum safety factor."""
+    min_safety_factor = float("inf")
+    best_second = None
+
+    for t in range(max_seconds + 1):
+        positions = simulate_robot_positions(robots, t, width, height)
+        quadrants = count_quadrants(positions, width, height)
+        safety_factor = calculate_safety_factor(quadrants)
+
+        if safety_factor < min_safety_factor:
+            min_safety_factor = safety_factor
+            best_second = t
+
+    return best_second
+
+
+def part2(filepath, max_seconds, width, height):
+    robots = parse_file(filepath)
+    return find_minimum_safety_factor(robots, max_seconds, width, height)
 
 
 if __name__ == "__main__":
-    print(f"P1 example (12): {part1('example',SECONDS,11,7)}")
+    print(f"P1 example (Expected: 12): {part1('example',SECONDS,11,7)}")
     print(
-        f"P1 example (218619324): {part1('puzzle_input',SECONDS,GRID_WIDTH,GRID_HEIGHT)}"
+        f"P1 puzzle_input (Expected: 218619324): {
+        part1('puzzle_input',SECONDS,GRID_WIDTH,GRID_HEIGHT)}"
+    )
+    print(
+        f"P2 puzzle_input (Expected: 6446): {
+        part2('puzzle_input', 7000, GRID_WIDTH,GRID_HEIGHT)}"
     )
